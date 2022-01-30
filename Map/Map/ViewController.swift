5e4b1fd415f5349addfb06bb2f0f5957b9dc6cf6
J.H.Moon <moonjh587@naver.com>
2022-01-30 23:55:02 +0900

    Modify Map project

--- Map/Map/ViewController.swift
+++ Map/Map/ViewController.swift
@@ -39,6 +39,9 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
     func setAnnotation(latitudeValue: CLLocationDegrees, longitudeVale : CLLocationDegrees, delta span :Double, title strTitle:String,subtitle strSubtitle:String){
         let annotation = MKPointAnnotation()
         annotation.coordinate=goLocation(latitudeValue: latitudeValue, longitudeValue: longitudeVale, delta: span)
+        annotation.title=strTitle
+        annotation.subtitle=strSubtitle
+        myMap.addAnnotation(annotation)
     }
     
     func locationManager(_ manager: CLLocationManager,didUpdateLocations locations:[CLLocation]){
@@ -65,6 +68,21 @@ class ViewController: UIViewController,CLLocationManagerDelegate {
         })
     }
     @IBAction func sgChangeLocation(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex==0{
+            self.lblLocationinfo1.text=""
+            self.lblLocationinfo2.text=""
+            locationManager.startUpdatingLocation()
+        }
+        else if sender.selectedSegmentIndex==1{
+            setAnnotation(latitudeValue: 37.751853, longitudeVale: 128.87605740000004, delta: 1, title: "한국폴리텍대학 강릉캠퍼스", subtitle: "강원도 강릉시 남산초교길 121")
+            self.lblLocationinfo1.text="보고 계신 위치"
+            self.lblLocationinfo2.text="한국폴리텍대학 강릉캠퍼스"
+        }
+        else if sender.selectedSegmentIndex==2{
+            setAnnotation(latitudeValue: 37.556876, longitudeVale: 126.914066, delta: 0.1, title: "이지스퍼블리싱", subtitle: "서울시 마포구 잔다리로 109 이지스 빌딩")
+            self.lblLocationinfo1.text="보고 계신 위치"
+            self.lblLocationinfo2.text="이지스퍼블리싱 출판사"
+        }
         
     }
     
